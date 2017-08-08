ade25a102750a7bdf02ff87cb679e5e06f4772eb
Corey Dixon <zimhey1@gmail.com>
2017-08-08 11:45:49 +0900

    Maze Generator skeleton classes

---
+++ Assets/Scripts/MazeNode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeNode : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public MazeNode Left;
+    public MazeNode Right;
+    public MazeNode Forward;
+    public MazeNode Backward;
+    public MazeNode Above;
+    public MazeNode Below;
+
+    public bool Bottleneck;
+
+    public int Row;
+    public int Col;
+
+}
---
+++ Assets/Scripts/RecursiveMazeGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecursiveMazeGenerator : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public static MazeNode GenerateMaze(int seed);
+}
