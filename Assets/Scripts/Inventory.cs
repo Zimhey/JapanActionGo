573f42ba7d7480baaf5c7ea0870130f09401e8ae
Corey Dixon <zimhey1@gmail.com>
2017-08-09 18:03:49 +0900

    Added Pickup and Inventory Class and ItemType enum

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public int ChalkCharges;
+    public int OfudaCharges;
+    public bool hasMirror;
+    public bool hasCompass;
+
+    public void Found(ItemType item)
+    {
+        switch(item)
+        {
+            case ItemType.Chalk:
+                ChalkCharges++;
+                break;
+            case ItemType.Ofuda:
+                OfudaCharges++;
+                break;
+            case ItemType.Mirror:
+                hasMirror = true;
+                break;
+            case ItemType.Compass:
+                hasCompass = true;
+                break;
+        }
+    }
+
+    public void Used(ItemType item)
+    {
+        switch(item)
+        {
+            case ItemType.Chalk:
+                ChalkCharges--;
+                break;
+            case ItemType.Ofuda:
+                OfudaCharges--;
+                break;
+        }
+    }
+
+    public bool CanUse(ItemType item)
+    {
+        switch(item)
+        {
+            case ItemType.Chalk:
+                return ChalkCharges > 0;
+            case ItemType.Ofuda:
+                return OfudaCharges > 0;
+            case ItemType.Mirror:
+                return hasMirror;
+            case ItemType.Compass:
+                return hasCompass;
+        }
+        return false;
+    }
+}
