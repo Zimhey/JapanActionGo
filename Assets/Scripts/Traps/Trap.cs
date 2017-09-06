be85f14937dd01565f680a8252421b658ee1a412
Corey Dixon <zimhey1@gmail.com>
2017-09-06 16:17:05 +0900

    Added Hold Time and moved everything into the Trap class

--- Assets/Scripts/Traps/Trap.cs
+++ Assets/Scripts/Traps/Trap.cs
@@ -14,6 +14,7 @@ public class Trap : MonoBehaviour
 {
     public TrapState state;
     public float FireAnimTime;
+    public float HoldAnimTime;
     public float ResetAnimTime;
     // public float TriggerDelay;
     public bool CanReset;
@@ -23,6 +24,11 @@ public class Trap : MonoBehaviour
     protected float activationTime;
 
     private Actor actorID;
+
+    public Vector3 ArmedPosition;
+    public Vector3 ArmedRotation;
+    public Vector3 UnarmedPosition;
+    public Vector3 UnarmedRotation;
 
     public TrapState State
     {
@@ -42,7 +48,24 @@ public class Trap : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-	}
+        switch (state)
+        {
+            case TrapState.Armed:
+                gameObject.transform.localPosition = ArmedPosition;
+                ResetRequested = false;
+                break;
+            case TrapState.Firing:
+                PlayFireAnimation();
+                break;
+            case TrapState.Unarmed:
+                gameObject.transform.localPosition = UnarmedPosition;
+                tryReset();
+                break;
+            case TrapState.Resetting:
+                PlayResetAnimation();
+                break;
+        }
+    }
 
     public void TriggerTrap()
     {
@@ -54,10 +77,47 @@ public class Trap : MonoBehaviour
 
     protected void tryReset()
     {
-        if (CanReset && ResetRequested)
+        if (CanReset && ResetRequested && Time.time > activationTime + HoldAnimTime)
         {
             activationTime = Time.time;
             state = TrapState.Resetting;
+        }
+    }
+
+    void PlayFireAnimation()
+    {
+        float complete = (Time.time - activationTime) / FireAnimTime;
+        if (complete < 1.0)
+        {
+            gameObject.transform.localPosition = Vector3.Lerp(ArmedPosition, UnarmedPosition, complete);
+        }
+        else
+        {
+            activationTime = Time.time;
+            state = TrapState.Unarmed;
+        }
+            
+    }
+
+    void PlayResetAnimation()
+    {
+        float complete = (Time.time - activationTime) / ResetAnimTime;
+        if (complete < 1.0)
+        {
+            gameObject.transform.localPosition = Vector3.Lerp(UnarmedPosition, ArmedPosition, complete);
+        }
+        else
+            state = TrapState.Armed;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject != null)
+        {
+            Debug.Log(gameObject.name + " Kill " + collider.gameObject.name);
+            GameManager.Instance.ActorKilled(collider.gameObject.GetComponent<Actor>(), GetComponentInParent<Actor>());
+            print("killing" + collider.gameObject);
+            collider.gameObject.SendMessage("Die");
         }
     }
 }
