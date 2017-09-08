b2298377d15a7aad35c6ecac8b97d4978f578c1b
prmalone2 <prmalone2@wpi.edu>
2017-09-08 16:09:49 +0900

    started menu code

---
+++ Assets/Scripts/UI/KeyBindingScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyBindingScript : MonoBehaviour {
+
+    private Dictionary<string, KeyCode> buttons = new Dictionary<string, KeyCode>();
+
+    public Text run, jump, chalk, ofuda;
+
+	// Use this for initialization
+	void Start () {
+        buttons.Add("Run", KeyCode.LeftShift);
+        buttons.Add("Jump", KeyCode.Space);
+        buttons.Add("Chalk", KeyCode.Mouse0);
+        buttons.Add("Throw", KeyCode.Mouse1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
