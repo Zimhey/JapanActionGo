3abc3fa39b0c66c928215bc21ac8c7d0682ff763
prmalone2 <prmalone2@wpi.edu>
2017-09-13 11:34:23 +0900

    added some control stuff for other controllers

--- Assets/Scripts/UI/KeyBindingScript.cs
+++ Assets/Scripts/UI/KeyBindingScript.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 public class KeyBindingScript : MonoBehaviour {
 
     public static Dictionary<string, KeyCode> buttons = new Dictionary<string, KeyCode>();
+    public static Dictionary<string, KeyCode> controller = new Dictionary<string, KeyCode>();
+    public static Dictionary<string, KeyCode> vr = new Dictionary<string, KeyCode>();
 
-    public Text run, jump, chalk, ofuda;
+    public Text run, jump, chalk, ofuda, c_run, c_jump, c_chalk, c_ofuda, vr_run, vr_jump, vr_chalk, vr_ofuda;
 
     private GameObject currentKey;
 
@@ -22,6 +24,26 @@ public class KeyBindingScript : MonoBehaviour {
         jump.text = buttons["Jump"].ToString();
         chalk.text = buttons["Draw"].ToString();
         ofuda.text = buttons["Throw"].ToString();
+
+        controller.Add("Run", KeyCode.LeftShift);
+        controller.Add("Jump", KeyCode.Space);
+        controller.Add("Draw", KeyCode.Mouse0);
+        controller.Add("Throw", KeyCode.Mouse1);
+
+        c_run.text = buttons["Run"].ToString();
+        c_jump.text = buttons["Jump"].ToString();
+        c_chalk.text = buttons["Draw"].ToString();
+        c_ofuda.text = buttons["Throw"].ToString();
+
+        vr.Add("Run", KeyCode.LeftShift);
+        vr.Add("Jump", KeyCode.Space);
+        vr.Add("Draw", KeyCode.Mouse0);
+        vr.Add("Throw", KeyCode.Mouse1);
+
+        vr_run.text = buttons["Run"].ToString();
+        vr_jump.text = buttons["Jump"].ToString();
+        vr_chalk.text = buttons["Draw"].ToString();
+        vr_ofuda.text = buttons["Throw"].ToString();
     }
 	
 	// Update is called once per frame
