754feff8e5a7f82ebc0df9bc9c27552ea4c65e40
Corey Dixon <zimhey1@gmail.com>
2017-09-19 16:36:58 +0900

    Compass Fix

--- Assets/Scripts/Player/CompassPin.cs
+++ Assets/Scripts/Player/CompassPin.cs
@@ -6,13 +6,10 @@ public class CompassPin : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Quaternion quat = Quaternion.FromToRotation(transform.parent.rotation.eulerAngles, Vector3.forward);
-        transform.rotation = quat;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Quaternion quat = Quaternion.FromToRotation(transform.parent.rotation.eulerAngles, Vector3.forward);
-        transform.rotation = quat;
-	}
+        transform.localRotation = Quaternion.Euler(0, 360 - transform.parent.rotation.eulerAngles.y, 0);
+    }
 }
