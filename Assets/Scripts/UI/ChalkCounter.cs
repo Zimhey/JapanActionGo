8fdbbc89dffb7df25f4efc904d96b708441f7dfc
Wakelley <wakelley@wpi.edu>
2017-11-29 21:43:56 -0500

    vr hud stuff

--- Assets/Scripts/UI/ChalkCounter.cs
+++ Assets/Scripts/UI/ChalkCounter.cs
@@ -9,24 +9,41 @@ public class ChalkCounter : MonoBehaviour {
     public GameObject player;
 
     public Text chalkLabel;
-    public Text chalkLabel2;
+    public GameObject chalkLabelVR;
 
     private Inventory playerInventory;
     private PlayerActions playerActions;
+    private TextMesh VRCounter;
 
     // Use this for initialization
     void Start () {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
 
+        if (player != null)
+        {
+            playerInventory = player.GetComponent<Inventory>(); // TODO GROSS FIX THIS LETS NOT GRAB THE COMPONENT EVERY FRAME
+            playerActions = player.GetComponent<PlayerActions>();
+        }
+
+        if (chalkLabelVR != null)
+        {
+            VRCounter = chalkLabelVR.GetComponentInChildren<TextMesh>();
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
-        player = GameObject.FindGameObjectWithTag("Player");
-        playerInventory = player.GetComponent<Inventory>(); // TODO GROSS FIX THIS LETS NOT GRAB THE COMPONENT EVERY FRAME
-        playerActions = player.GetComponent<PlayerActions>();
-
-        chalkLabel.text = "Chalk: " + playerInventory.ChalkCharges * 100 + playerActions.DistanceDrawn / playerInventory.DistancePerCharge * 100;
-
+        if (chalkLabel != null)
+        {
+            chalkLabel.text = "Chalk: " + playerInventory.ChalkCharges * 100 + playerActions.DistanceDrawn / playerInventory.DistancePerCharge * 100;
+        }
+        if (chalkLabelVR != null)
+        {
+            VRCounter.text = "" + playerInventory.ChalkCharges * 100 + playerActions.DistanceDrawn / playerInventory.DistancePerCharge * 100;
+        }
         /*
         //whole charges
         if (playerInventory.ChalkCharges > 0 && playerActions.DistanceDrawn > 0)
